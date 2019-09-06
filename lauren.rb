c1fe7708c19f5c5123ce59ca587173174bb2c3ca
sqdorte <sqd0rte@Protonmail.com>
2019-09-06 15:15:06 -0300

    some code updates

--- lauren.rb
+++ lauren.rb
@@ -2,21 +2,19 @@ include Math
 
 class Neuron
 
-  @@value = 0
-  @@weights = []
-  @@bias = 0
-
   def sigmoid(x)
     1/(1+Math::E**x)
   end
 
-  def initialize(lastLayerCount)
-    @@weights = Array.new(lastLayerCount) { rand(1..1000)/100.0 } 
-    @@bias = rand(1.1000)/100.0
+  def initialize(lastLayerCount=0)
+    if lastLayerCount != 0
+      @weights = Array.new(lastLayerCount) { rand(1..1000)/100.0 } 
+      @bias = rand(1.1000)/100.0
+    end
   end
 
   def update(lastLayer)
-    @@value = sigmoid( (weights.zip(lastLayer).map { |weight, neuron| weight*neuron }).sum + @@bias )
+   @value = sigmoid((@weights.zip(lastLayer).map { |weight, neuron| weight*neuron }).sum + @bias)
   end
 
 end
